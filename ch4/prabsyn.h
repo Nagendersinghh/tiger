f2f2e5598c4fb9f27039726930c64e42ff3855f7
Nagender Singh <nagendersinghh@gmail.com>
2017-06-24 20:02:35 +0530

    Create Abstract Syntax Tree for tiger

---
+++ ch4/prabsyn.h
@@ -0,0 +1,7 @@
+/* function prototype from prabsyn.c */
+void pr_exp(FILE *out, A_exp v, int d);
+void pr_dec(FILE *out, A_dec v, int d);
+void pr_ty(FILE *out, A_ty v, int d);
+void pr_field(FILE *out, A_field v, int d);
+void pr_fieldList(FILE *out, A_fieldList v, int d);
+
